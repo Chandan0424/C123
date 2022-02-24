abfa0d016189928adc502997640fdef0d8222fe1
Chandan0424 <76838260+Chandan0424@users.noreply.github.com>
2022-02-24 17:27:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function preload(){}
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(400,600);
+    video.position(100,100)
+}
+function draw(){}
