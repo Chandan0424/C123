53c1c92b2a03a5a625edeb58162a504e4f14db8b
Chandan0424 <76838260+Chandan0424@users.noreply.github.com>
2022-03-03 17:33:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.size(400,600);
-    video.position(100,100)
+    video.position(100,100);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('Posenet is Loaded!');
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
 }
 function draw(){}
